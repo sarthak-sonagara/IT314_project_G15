cafe261743627a6b25f032c7009dd050dc8271cb
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-04-01 22:27:31 +0530

    NODEJS backend for authentication

---
+++ V2/backend/server.js
@@ -0,0 +1,32 @@
+// config to dotenv
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/userRoutes");
+
+// express app
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/auth/user", userRoutes);
+
+// connect to mongodb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
