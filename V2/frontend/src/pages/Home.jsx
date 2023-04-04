ee864b8d5f2e5eda5e010a935436d70d53db9a5d
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-04-04 18:20:45 +0530

    Frontend changed to reactjs

---
+++ V2/frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <>
+    <div>
+        <h1>Home</h1>
+        <h2>we are group 15</h2>
+    </div>
+   </>
+  )
+}
+
+export default Home
