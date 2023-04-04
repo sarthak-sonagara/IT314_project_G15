c759c5f1a67ee6993e2dedc9b8359cf4f23c9941
parth-1205 <parthprajapati97239@gmail.com>
2023-04-05 00:05:35 +0530

    Organization Signup/Login API Testing and bug fixing

--- V2/backend/models/org.js
+++ V2/backend/models/org.js
@@ -3,111 +3,112 @@ const bcyrpt = require("bcrypt");
 const validator = require("validator");
 
 const orgSchema = new mongoose.Schema({
-  orgname: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
+    orgname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
 
 });
 
 // static signup function
 orgSchema.statics.signup = async function (orgname, email, password) {
-  // validation
-  if (!orgname || !email || !password) {
-    throw new Error("Please fill all the fields");
-  }
+    // validation
+    if (!orgname || !email || !password) {
+        throw new Error("Please fill all the fields");
+    }
 
-  if (!validator.isEmail(email)) {
-    throw new Error("Invalid email");
-  }
+    if (!validator.isEmail(email)) {
+        throw new Error("Invalid email");
+    }
 
-  if (!validator.isStrongPassword(password)) {
-    throw new Error(
-      "Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 symbol"
-    );
-  }
+    if (!validator.isStrongPassword(password)) {
+        throw new Error(
+            "Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 symbol"
+        );
+    }
 
-  const exist = await this.findOne({ email });
+    const exist = await this.findOne({ email });
 
-  if (exist) {
-    throw new Error("Email already exists");
-  }
+    if (exist) {
+        throw new Error("Email already exists");
+    }
 
-  //   hasing and creating new organization
-  const salt = await bcyrpt.genSalt(10);
-  const hash = await bcyrpt.hash(password, salt);
+    //   hasing and creating new organization
+    const salt = await bcyrpt.genSalt(10);
+    const hash = await bcyrpt.hash(password, salt);
 
-  const org = await this.create({
-    orgname,
-    email,
-    password: hash,
-  });
+    const org = await this.create({
+        orgname,
+        email,
+        password: hash,
+    });
 
-  return org;
+    return org;
 };
 
 
 // static login function
 
 orgSchema.statics.login = async function (email, password) {
-  
+
     // validation
-  if (!email || !password) {
-    throw new Error("Please fill all the fields");
-  }
+    if (!email || !password) {
+        throw new Error("Please fill all the fields");
+    }
 
-  const org = await this.findOne({ email });
+    const org = await this.findOne({ email });
 
-  //if organization's email does not exist, so first we have to sign up.
-  if (!org) {
-    throw new Error("Email or password is incorrect");
-  }
+    //if organization's email does not exist, so first we have to sign up.
+    if (!org) {
+        throw new Error("Email or password is incorrect");
+    }
 
-  const match = await bcyrpt.compare(password, org.password);
+    const match = await bcyrpt.compare(password, org.password);
 
-  if (!match) {
-    throw new Error("Email or password is incorrect");
-  }
+    if (!match) {
+        throw new Error("Email or password is incorrect");
+    }
 
 
-  return org;
+    return org;
 };
 
 
 // static update function
 orgSchema.statics.update = async function (email, password) {
-  
+
     // validation
-  if (!email || !password) {
-    throw new Error("Please fill all the fields");
-  }
-  
-  const org = await this.findOne({ email });
+    if (!email || !password) {
+        throw new Error("Please fill all the fields");
+    }
 
-  if (!org) {
-    throw new Error("Not authorized to update");
-  }
+    const org = await this.findOne({ email });
 
-  const salt = await bcyrpt.genSalt(10);
-  const hash = await bcyrpt.hash(password, salt);
-  
-  org.password = hash; // update password
+    if (!org) {
+        throw new Error("Not authorized to update");
+    }
 
-  await org.save();
-  return org;
+    const salt = await bcyrpt.genSalt(10);
+    const hash = await bcyrpt.hash(password, salt);
+
+    org.password = hash; // update password
+
+    await org.save();
+    return org;
 };
 
 
