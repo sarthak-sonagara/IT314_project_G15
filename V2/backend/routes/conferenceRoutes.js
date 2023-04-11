ee9aac44907dcbd50178c8ff3f30ef77b7ab411b
parth-1205 <parthprajapati97239@gmail.com>
2023-04-12 00:47:05 +0530

    add Update and Delete conference functionality for Organizations

--- V2/backend/routes/conferenceRoutes.js
+++ V2/backend/routes/conferenceRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 
 router.route("/createConference").post(conferenceController.createConference);
 
+router.route("/editConference").put(conferenceController.editConference);
+
+router.route("/deleteConference").delete(conferenceController.deleteConference);
 
 module.exports = router;
