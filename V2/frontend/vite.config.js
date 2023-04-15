83938abce6a19e6c9c5af25d7024974a65005a43
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-04-15 17:14:20 +0530

    test

--- V2/frontend/vite.config.js
+++ V2/frontend/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+console.log(process.env.NODE_ENV);
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/auth/": {
-        target:
-          process.env.NODE_ENV === "development"
-            ? "http://localhost:3000"
-            : "https://conf-backend.onrender.com",
+        target: process.env.NODE_ENV
+          ? "http://localhost:3000"
+          : "https://conf-backend.onrender.com",
         changeOrigin: true,
         secure: false,
       },
