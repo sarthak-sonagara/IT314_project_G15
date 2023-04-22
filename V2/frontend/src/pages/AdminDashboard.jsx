303595e6201c11070e3bb779eca7cf778e80a300
b202001162 <202001162@daiict.ac.in>
2023-04-22 17:33:44 +0530

    Admin integration completed

--- V2/frontend/src/pages/AdminDashboard.jsx
+++ V2/frontend/src/pages/AdminDashboard.jsx
@@ -1,27 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/CSS/style.css";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+//jQuery libraries
+import "jquery/dist/jquery.min.js";
+//Datatable Modules
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import $ from "jquery";
 import Table from "react-bootstrap/Table";
-import {
-  faArrowsLeftRightToLine,
-  faBars,
-  faBarsStaggered,
-  faBell,
-  faClipboardList,
-  faHome,
-  faRightFromBracket,
-  faSearch,
-  faServer,
-  faSignOut,
-  faTimeline,
-  faUpload,
-  faUser,
-  faUserAlt,
-  faUserGroup,
-} from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faSignOut } from "@fortawesome/free-solid-svg-icons";
 
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
@@ -33,6 +22,19 @@ const AdminDashboard = () => {
   const handleClick = () => {
     logout();
   };
+
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch("http://localhost:3000/auth/user/")
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
   return (
     <>
       <nav className="nav-bar admin-nav">
@@ -78,49 +80,58 @@ const AdminDashboard = () => {
       </nav>
       <div className="admin-container">
         <div className="admin-content-ctn">
-          <Table
-            id="example" className="table table-striped" 
-            striped
-            bordered
-            hover
-            size="sm"
-          >
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
+          <div className="container">
+            <table id="example" class="display">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Operations</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>ID</th>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Operations</th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-$(document).ready(function () {
-  $("#example").DataTable();
-});
+var xmlhttp = new XMLHttpRequest();
+var listFilesUrl = "http://localhost:3000/auth/user/";
+xmlhttp.open("GET", listFilesUrl, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function () {
+  if(this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+    let table = $("#example").DataTable({
+      stateSave: true,
+    "bDestroy": true,
+      "data": data.users,
+      "columns": [
+        { data: "_id" },
+        { data: "username" },
+        { data: "email" },
+        { data: "role" },
+        {
+          data: null,
+          defaultContent: "<i class='fa-solid fa-pen-to-square'></i>"
+        }
+      ],
+    });
+  }
+}
 
 export default AdminDashboard;
