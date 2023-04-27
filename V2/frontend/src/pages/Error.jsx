d6c37de17f28b64d87d81a768b52f68aa489fd86
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-04-27 18:06:05 +0530

    Link added to home

--- V2/frontend/src/pages/Error.jsx
+++ V2/frontend/src/pages/Error.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-
     <div className="d-flex align-items-center justify-content-center vh-100">
       <div className="text-center">
         <h1 className="display-1 fw-bold">404</h1>
-        <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
-        <p className="lead">
-          The page you’re looking for doesn’t exist.
+        <p className="fs-3">
+          {" "}
+          <span className="text-danger">Opps!</span> Page not found.
         </p>
-        <a className="btn btn-primary">Go Home</a>
+        <p className="lead">The page you’re looking for doesn’t exist.</p>
+        <Link to="/" className="btn btn-primary">
+          Go Home
+        </Link>
       </div>
     </div>
   );
