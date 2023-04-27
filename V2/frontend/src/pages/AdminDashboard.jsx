66f701fa23ab77b06a2b847afaa0805c33771756
b202001162 <202001162@daiict.ac.in>
2023-04-27 19:44:39 +0530

    Trash button added to dashboard

--- V2/frontend/src/pages/AdminDashboard.jsx
+++ V2/frontend/src/pages/AdminDashboard.jsx
@@ -113,7 +113,7 @@ const AdminDashboard = () => {
           {
             data: null,
             defaultContent:
-              '<div style="display:flex"><button class="btn btn-danger delete-btn" id="adm-dashboard-button-size">Delete</button></div>',
+              '<div style="display:flex"><svg class="delete-btn" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></div>',
           },
         ],
       });
@@ -153,7 +153,7 @@ const AdminDashboard = () => {
           {
             data: null,
             defaultContent:
-              '<div style="display:flex"><button class="btn btn-danger delete-btn" id="adm-dashboard-button-size">Delete</button></div>',
+              '<div style="display:flex"><svg class="delete-btn" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></div>',
           },
         ],
       });
@@ -185,7 +185,6 @@ const AdminDashboard = () => {
   return (
     <>
       <AdminNavbar />
-      {/* <Tabs/> */}
       <div className="admin-nav-left-span">
         <Link className="admin-nav-left-sub-ctn" to="/">
           <p
